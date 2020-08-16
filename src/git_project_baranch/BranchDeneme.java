dd8b35d91ffd2fdff591e0f92651d42187483b27
unknown <mehmetkoksal61@gmail.com>
2020-08-16 18:55:07 +0200

    featureden ikinci commit

--- src/git_project_baranch/BranchDeneme.java
+++ src/git_project_baranch/BranchDeneme.java
@@ -5,6 +5,7 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 		System.out.println("18:29 - master degisiklik");
 		System.out.println("20-36 - future degisiklik");
+		System.out.println("20-36 - future ikinci degisiklik");
 
 	}
 

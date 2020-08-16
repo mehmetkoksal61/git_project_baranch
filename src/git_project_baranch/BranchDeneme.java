875879a838fd53ad9b18b65fb0aa60f66f2e4cdc
unknown <mehmetkoksal61@gmail.com>
2020-08-16 18:39:10 +0200

    featureden commit

--- src/git_project_baranch/BranchDeneme.java
+++ src/git_project_baranch/BranchDeneme.java
@@ -4,6 +4,7 @@ public class BranchDeneme {
 
 	public static void main(String[] args) {
 		System.out.println("18:29 - master degisiklik");
+		System.out.println("20-36 - future degisiklik");
 
 	}
 
